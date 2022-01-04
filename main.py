a4aca028e4c7ca11c4395adcb65b658dbbf65511
Jeff Cherry <jeff_wired@yahoo.com>
2022-01-04 06:21:53 -0800

    Dewpoint and rainfall added

--- main.py
+++ main.py
@@ -54,11 +54,42 @@ e = a.find("<", d)
 g = a[d+1:e]
 current_wind_gust = g
 
+# Find current total precipitation
+b = a.find(">PRECIP ACCUM<")
+c = a.find("wu-value wu-value-to", b)
+d = a.find(">", c)
+e = a.find("<", d)
+g = a[d+1:e]
+current_total_precipitation = g
 
+# Find current total precipitation rate
+b = a.find(">PRECIP RATE<")
+c = a.find("wu-value wu-value-to", b)
+d = a.find(">", c)
+e = a.find("<", d)
+g = a[d+1:e]
+current_total_precipitation_rate = g
 
+# Find current dewpoint
+b = a.find(">DEWPOINT<")
+c = a.find("wu-value wu-value-to", b)
+d = a.find(">", c)
+e = a.find("<", d)
+g = a[d+1:e]
+current_dewpoint = g
 print(b, c, d, e, g)
 
-print(current_temperature, current_pressure, current_humidity, current_wind_speed, current_wind_gust)
+print('Current Temperature', current_temperature, 'F')
+print('Current Dewpoint', current_dewpoint, 'F')
+print('Current Pressure', current_pressure, 'in/hg')
+print('Current Humidity', current_humidity, '%')
+print('Current Wind Speed', current_wind_speed, 'MPH')
+print('Current Wind Gust', current_wind_gust, 'MPH')
+print('Current Total Precipitation', current_total_precipitation, 'in')
+print('Current Total Precipitation Rate', current_total_precipitation_rate, 'in/hr')
+
+
+
 
 
 
