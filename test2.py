2bd5f5d6bd0e2400a28dd05bedbb55e7789f163b
Jeff Cherry <jeff_wired@yahoo.com>
2023-04-04 09:54:35 -0700

    Added rsm page

--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
@@ -6,10 +7,10 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.utils.extmath import safe_sparse_dot
 
 x1 = np.array([1, 1.1, 1.1, 1.2, 1.2])
-x2 = np.array([2,2,2,2,2])
-x3 = np.array([3, 3, 3, 3, 3])
-x4 = np.array([4, 4, 4, 4, 4])
-y = np.array([2,2.1,2.1,2.2,2.6])
+x2 = np.array([2.2,2,2.3,2,2])
+x3 = np.array([3, 3.2, 3.1, 3.3, 3.4])
+x4 = np.array([4, 4.1, 4, 4.3, 4])
+y = np.array([2,2.1,2.8,2.2,2.6])
 
 # Create the design matrix
 X = np.column_stack((x1, x2, x3, x4))
@@ -26,7 +27,7 @@ coeff = model.coef_
 print(model.intercept_)
 print(model.coef_)
 
-n = 0
+n = 1
 
 x1 = x1[n]
 x2 = x2[n]
@@ -34,14 +35,14 @@ x3 = x3[n]
 x4 = x4[n]
 y = y[n]
 
--0.33 + 0.17x1 + 0.25x2 + 0.08x3 + 0.01x1^2 + 0.05x1x2 -0.01x1x3 + 0.03x2^2 + 0.01x2x3 + 0.00x3^2 -0.03x1x4 + 0.02x2x4 + 0.00x3x4 + 0.02x4^2
+# ['1', 'x1', 'x2', 'x3', 'x4', 'x1^2', 'x1 x2', 'x1 x3', 'x1 x4', 'x2^2', 'x2 x3', 'x2 x4', 'x3^2', 'x3 x4', 'x4^2']
 
 
-row = intercept \
+row = intercept + coeff[0] \
     + coeff[1]*x1 + coeff[2]*x2  + coeff[3]*x3 + coeff[4]*x4 \
-    + coeff[5]*x1*x1 + coeff[6]*x2*x2 + coeff[7]*x3*x3 + coeff[8]*x4*x4 \
-    + coeff[9]*x1*x2 + coeff[10]*x1*x3 + coeff[11]*x1*x4 \
-    + coeff[12]*x2*x3 + coeff[13]*x2*x4 + coeff[14]*x3*x4
+    + coeff[5]*x1*x1 + coeff[6]*x1*x2 + coeff[7]*x1*x3 + coeff[8]*x1*x4 \
+    + coeff[9]*x2*x2 + coeff[10]*x2*x3 + coeff[11]*x2*x4 \
+    + coeff[12]*x3*x3 + coeff[13]*x3*x4 + coeff[14]*x4*x4
 
 print(row, x1,x2,x3,x4,y)
 
@@ -50,8 +51,6 @@ print(row, x1,x2,x3,x4,y)
 # + {coeff[3]:.2f}x4 + {coeff[4]:.2f}x1^2 + {coeff[5]:.2f}x2^2 + {coeff[6]:.2f}x3^2 + {coeff[7]:.2f}x4^2\
  #   + {coeff[8]:.2f}x1*X2 + {coeff[9]:.2f}x1*x3 + {coeff[10]:.2f}x1*x3 + {coeff[11]:.2f}x1*x4\
 #    + {coeff[12]:.2f}x2*x3 + {coeff[13]:.2f}x2*x4 + {coeff[14]:.2f}x3*x4')
-
-
 
 
 
--- test4.py
+++ test4.py
@@ -1,42 +1,14 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+import pandas as pd
+import numpy as np
 
-# Generate random data with 4 independent variables and 1 dependent variable
-np.random.seed(0)
-X = np.random.rand(100, 4)
-y = 2 * X[:, 0] + 3 * X[:, 1] - 4 * X[:, 2] ** 2 + 5 * X[:, 3] ** 3 + np.random.normal(0, 0.5, 100)
+data = pd.DataFrame.from_dict({
+    'x1': np.random.randint(low=1, high=10, size=5),
+    'x2': np.random.randint(low=1, high=10, size=5),
+    'x3': np.random.randint(low=1, high=10, size=5),
+    'x4': np.random.randint(low=1, high=10, size=5),
+    #'y': np.random.randint(low=-1, high=1, size=5),
+})
 
-# Fit a polynomial regression model with degree=2
-poly = PolynomialFeatures(degree=2)
-X_poly = poly.fit_transform(X)
-model = LinearRegression().fit(X_poly, y)
-
-# Get the intercept and coefficients
-intercept = model.intercept_
-coefficients = model.coef_
-
-print(intercept)
-print(coefficients)
-
-# Create the polynomial regression equation string
-variables = ['X1', 'X2', 'X3', 'X4']
-terms = []
-for i, c in enumerate(coefficients):
-    if i == 0:
-        terms.append('{:.2f}'.format(c))
-    else:
-        term = ''
-        for j, p in enumerate(np.where(c != 0)[0]):
-            if p == 0:
-                term += '{:.2f} + '.format(intercept)
-            else:
-                coef = '{:.2f}'.format(c[p])
-                powers = ' '.join(['{}^{}'.format(variables[k], int(poly.powers_[p, k])) for k in range(4)])
-                term += '{} {} + '.format(coef, powers)
-        term = term[:-3]
-        terms.append(term)
-
-equation = 'y = ' + ' + '.join(terms)
-print(equation)
-# y = 0.07 - 0.39 X1 + 0.78 X2 - 8.21 X3^2 + 5.23 X4^3 - 0.33 X1 X2 - 0.17 X1 X3 + 0.23 X1 X4 + 0.03 X2 X3 - 0.06 X2 X4 - 0.06 X3 X4 + 0.21 X1^2 - 0.09 X2^2 - 0.31 X3^2 + 0.24 X4^2
+p = PolynomialFeatures(degree=2).fit(data)
+print(p.get_feature_names(data.columns))
