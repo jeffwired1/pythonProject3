42cb7ff318ff82e4e4f898c4ff7ab74aef581266
Jeff Cherry <jeff_wired@yahoo.com>
2023-05-18 05:28:30 -0700

    Added rsm page

--- rse_functions.py
+++ rse_functions.py
@@ -1,11 +1,13 @@
-import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+import openpyxl
+import tkinter as tk
+from PIL import Image, ImageTk
 
-def iterate1(x1,x2,x3,x4,y,input1,input2,input3,input4,inputy):
+def iterate1(x1, y, x_values):
     # Define the RSE parameters
-    X = np.column_stack((x1, x2, x3, x4))
+    X = np.column_stack((x1))
     poly = PolynomialFeatures(degree=2)
     X_design = poly.fit_transform(X)
 
@@ -18,7 +20,7 @@ def iterate1(x1,x2,x3,x4,y,input1,input2,input3,input4,inputy):
     coeff = model.coef_
 
     # Get feature names
-    feat_names = poly.get_feature_names_out([input1, input2, input3, input4])
+    feat_names = poly.get_feature_names_out(x_values)
 
     # Generate RSE equation for export
     equation = f"{intercept:.15f} + "
@@ -26,10 +28,99 @@ def iterate1(x1,x2,x3,x4,y,input1,input2,input3,input4,inputy):
         feat_names[i] = feat_names[i].replace(" ", " * ")
         equation = equation + str(coeff[i]) + " * " + feat_names[i] + " + "
     equation = equation[:-3]  # Remove the last " + "
-    equ = "(" + equation + ")"
+    equ = "( " + equation + " )"
+
+    # Replace equation terms with '^' as this is not valid in Python.
+    equ1 = equ
+    while equ1.find('^') != -1:
+        loc = equ1.find('^')
+        d = equ1.rfind(" ",0,loc)
+        e = equ1.find(" ", d+1)
+        r = equ1[d+1:e]
+        r1 = equ1[d+1:e-2]
+        r2 = equ1[loc+1:e]
+        r3 = int(r2)
+        str1 = r1
+        # Add number of multiply terms based on power
+        for x in range(r3-1):
+            str1 += " * " + r1
+        # Replace '^' terms in equation with solution
+        equ1 = equ1.replace(r, str1)
 
     # Get the RSE predictions
     rse = model.predict(X_design)
 
-    return equ, rse
+    # Insert actual values into equation to check equation syntax accuracy
+    equ2 = equ1
+    count = 0
+    for b in x_values:
+        equ2 = equ2.replace(" " + x_values[count] + " ", " " + str(X[0,count]) + " ")
+        count += 1
 
+    e = eval(equ2)
+    f = rse[0]
+    g = (e-f)/e
+    if abs(g) > 0.0001:
+        print(equ1)
+        print("Formula Error")
+        exit()
+
+
+    return equ1, rse
+
+
+def read_column(filename, sheetname, column_name):
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook[sheetname]
+    column_values = []
+
+    column_index = None
+    for cell in sheet[1]:  # Iterate through the first row to find the column index
+        if cell.value == column_name:
+            column_index = cell.column
+            break
+
+    if column_index is not None:
+        for i, row in enumerate(sheet.iter_rows(values_only=True), start=1):
+            if i > 1:  # Skip the first row
+                cell_value = row[column_index - 1]
+                if cell_value is not None:  # Ignore blank cells
+                    column_values.append(cell_value)
+
+    workbook.close()
+    return column_values
+
+
+def resize_image(image, max_width, max_height):
+    width, height = image.size
+    aspect_ratio = width / height
+
+    if width > max_width or height > max_height:
+        if width > height:
+            new_width = max_width
+            new_height = int(new_width / aspect_ratio)
+        else:
+            new_height = max_height
+            new_width = int(new_height * aspect_ratio)
+
+        resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+        return resized_image
+    else:
+        return image
+
+def create_image_window(image_files, grid_columns, max_width, max_height):
+    window = tk.Tk()
+    window.title("ALPHA vs RSE Check Plots")
+
+    total_images = len(image_files)
+    grid_rows = (total_images + grid_columns - 1) // grid_columns
+
+    for i, file in enumerate(image_files):
+        image = Image.open(file)
+        resized_image = resize_image(image, max_width, max_height)
+        photo = ImageTk.PhotoImage(resized_image)
+        label = tk.Label(window, image=photo)
+        label.image = photo  # Keep a reference to the image to prevent garbage collection
+        label.grid(row=i // grid_columns, column=i % grid_columns, padx=10, pady=10)
+
+    window.mainloop()
---
+++ run_rse.py
@@ -0,0 +1,144 @@
+import os
+import sys
+
+from matplotlib import pyplot as plt
+from rse_functions import *
+import pandas as pd
+from pathlib import Path
+from PyQt5.QtWidgets import QApplication, QFileDialog
+
+app = QApplication([])
+while 1:
+    # Open file dialog for ALPHA input file
+    input_file, _ = QFileDialog.getOpenFileName(None, "Open ALPHA Results File", "", "CSV Files (*.csv);;All Files (*.*)")
+    file_path = os.path.dirname(input_file)
+    config_path = os.path.join(file_path, "configuration.xlsx")
+
+    if not os.path.exists(input_file):
+        sys.exit()
+
+    # Read RSE input and output values from configuration file
+    x_values = read_column(config_path, 'Sheet1', 'RSE Inputs')
+    y_values = read_column(config_path, 'Sheet1', 'RSE Outputs')
+    print(x_values)
+    print(y_values)
+
+    # Clear arrays
+    equation = []
+
+    # Read the ALPHA file into dataframes
+    # Save the original file for output later
+    alpha_csv = pd.read_csv(input_file)
+    # Read again skipping second row of units
+    df=pd.read_csv(input_file, skiprows=[1])
+
+    # Get input data columns
+    x1 = []
+    count = 0
+    for x in x_values:
+        x1.append(df[x_values[count]])
+        count += 1
+
+    # Generate RSE equations iterating through all y values
+    count = 0
+    for x in y_values:
+        y = df[x]
+        equ, rse = iterate1(x1,y,x_values)
+
+        if count == 0:
+            count1 = 0
+            out = pd.DataFrame()
+            for b in x_values:
+                out.insert(count1,x_values[count1], x1[count1], True)
+                count1 += 1
+
+        # Add original and RSE output data to dataframe
+        out1 = pd.DataFrame(out)
+        out1[y_values[count] + "-ALPHA"] = y
+        out1[y_values[count] + "-RSE"] = rse
+        out = pd.DataFrame(out1)
+        # Generate check plot
+        out.plot(x=y_values[count] + "-ALPHA", y=y_values[count] + "-RSE", style='o', legend=None, color="black")
+        # Add titles to plot
+        font1 = {'family': 'arial', 'color': 'black', 'size': 20}
+        font2 = {'family': 'arial', 'color': 'black', 'size': 15}
+        plt.title(y_values[count], fontdict=font1)
+        plt.xlabel(y_values[count] + "-ALPHA", fontdict=font2)
+        plt.ylabel(y_values[count] + "-RSE", fontdict=font2)
+        # calculate equation for trendline
+        z = np.polyfit(out[y_values[count] + "-ALPHA"], out[y_values[count] + "-RSE"], 1)
+        p = np.poly1d(z)
+        # add trendline to plot
+        plt.plot(out[y_values[count] + "-ALPHA"], p(out[y_values[count] + "-ALPHA"]), color="black")
+        plt.savefig('plot' + str(count) + '.png')
+        plt.close()
+
+        # Add equation to array
+        equation.append(equ)
+
+        count += 1
+
+    # Create dataframe of equations
+    equation1 = pd.DataFrame({"Value" : y_values, "Equation" : equation})
+
+    #strip off .csv and add .xlsx to ALPHA filename
+    filename = Path(input_file)
+    filename = filename.with_suffix('')
+    filename = filename.with_suffix('.xlsx')
+
+    #Write out Excel workbook file with multiple worksheets
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+    # Write Equation worksheet
+    equation1.to_excel(writer, sheet_name='Equation')
+    worksheet = writer.sheets['Equation']
+    worksheet.autofit()
+    # Write out Data worksheet
+    out1.to_excel(writer, sheet_name='Data')
+    worksheet = writer.sheets['Data']
+    worksheet.autofit()
+    # Write original ALPHA data
+    alpha_csv.to_excel(writer, sheet_name='ALPHA_Input', index=False)
+    worksheet = writer.sheets['ALPHA_Input']
+    worksheet.autofit()
+    # Write out check plots
+    workbook  = writer.book
+    count = 0
+    for x in y_values:
+        workbook.add_worksheet('Plot ' + str(count))
+        worksheet = writer.sheets['Plot ' + str(count)]
+        plot_name = 'plot' + str(count) + '.png'
+        worksheet.insert_image('A1', plot_name)
+        count += 1
+
+    writer.close()
+
+    image_files = []
+    # Load check plot files
+    count = 0
+    for x in y_values:
+        plot_name = 'plot' + str(count) + '.png'
+        image_files.append(plot_name)
+        count += 1
+
+    # Display check plots
+    grid_columns = 4
+    max_width = 300
+    max_height = 300
+    create_image_window(image_files, grid_columns, max_width, max_height)
+
+    # Delete check plot files
+    for x in image_files:
+        if os.path.exists(x):
+            os.remove(x)
+
+    # Rename input file
+    on1 = input_file
+    fn1 = os.path.basename(on1)
+    fp1 = os.path.dirname(on1)
+    np1 = os.path.join(fp1, 'zzz_' + fn1)
+    # os.rename(on1, np1)
+
+
+# End script
+sys.exit()
+
