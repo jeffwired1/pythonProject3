5905949a2288d1fba179bd9680987134049da840
Jeff Cherry <jeff_wired@yahoo.com>
2023-07-19 16:49:46 -0700

    Arrays working

--- api.py
+++ api.py
@@ -6,20 +6,6 @@ API_KEY = '6d2447ce-f577-4896-bf2a-be8711735398'
 DEVICE_ID = '222373'
 STATION_ID = '84479'
 
-
-
-weatherflow_timestamp = 1626706051  # Replace with your actual WeatherFlow timestamp
-
-# Convert the timestamp to a datetime object
-dt = datetime.fromtimestamp(weatherflow_timestamp)
-
-# Extract the time component
-time_of_day = dt.time()
-
-print(time_of_day)
-
-
-
 def get_weather_data():
     url = f'https://swd.weatherflow.com/swd/rest/observations/device/{DEVICE_ID}?api_key={API_KEY}'
 
@@ -67,11 +53,10 @@ def get_forecast_data():
 #    print("Failed to retrieve weather data.")
 
 forecast_data = get_forecast_data()
-if forecast_data:
+if not forecast_data:
     # print(json.dumps(forecast_data, indent=4))
-    print("*******************************************************************")
-else:
     print("Failed to retrieve forecast data.")
+
 
 weatherflow_timestamp = forecast_data["current_conditions"]["time"]
 # Convert the timestamp to a datetime object
@@ -85,7 +70,7 @@ print(time_of_day)
 value = forecast_data["forecast"]["daily"]
 
 # individual_dicts = {}
-size = 25  # Size of the array
+size = 10  # Size of the array
 value2 = 0  # Initial value of the array elements
 forecast_array = [value2] * size
 
@@ -94,7 +79,9 @@ for item in value:
     forecast_array[x] = item
     x = x + 1
 
-print(forecast_array[9])
+for item in forecast_array:
+    print(item)
 
 d = forecast_array[9]["air_temp_low"]
 print(d)
+
