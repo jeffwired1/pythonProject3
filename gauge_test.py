bafba222c1583ee4081bb2ce41cf802f3ac5d0c2
Jeff Cherry <jeff_wired@yahoo.com>
2023-09-22 06:09:13 -0700

    Hourly and daily data accessible

--- gauge_test.py
+++ gauge_test.py
@@ -1,40 +1,43 @@
 import tkinter as tk
+import math
 
-class Gauge(tk.Canvas):
+class NeedleGauge(tk.Canvas):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs)
-        self.min_value = 0
-        self.max_value = 100
-        self.value = 0
         self.configure(width=200, height=200)
-        self.draw_gauge()
+        self.angle = 0  # Initial angle for the needle
+        self.needle_length = 80
+        self.needle_color = "red"
+        self.draw_needle()
 
-    def set_value(self, value):
-        if self.min_value <= value <= self.max_value:
-            self.value = value
-            self.draw_gauge()
+    def set_angle(self, angle):
+        self.angle = angle
+        self.draw_needle()
 
-    def draw_gauge(self):
-        self.delete("all")
-        angle = 240 * (self.value - self.min_value) / (self.max_value - self.min_value)
-        start_angle = 30
-        end_angle = 270
-        self.create_arc(10, 10, 190, 190, start=start_angle, extent=angle, fill="blue")
-        self.create_text(100, 100, text=f"{self.value}", font=("Arial", 20))
+    def draw_needle(self):
+        self.delete("needle")
+        center_x = self.winfo_reqwidth() / 2
+        center_y = self.winfo_reqheight() / 2
+
+        # Calculate the position of the needle
+        needle_angle = math.radians(self.angle)
+        needle_x = center_x + self.needle_length * math.cos(needle_angle)
+        needle_y = center_y + self.needle_length * math.sin(needle_angle)
+
+        # Draw the needle as a line
+        self.create_line(center_x, center_y, needle_x, needle_y, fill=self.needle_color, width=3, tags="needle")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Gauge")
+    root.title("Needle Gauge")
 
-    gauge = Gauge(root)
+    gauge = NeedleGauge(root)
     gauge.pack()
 
-    def update_gauge():
-        print("a")
-        gauge.set_value(gauge.value + 10)
-        root.after(1000, update_gauge)
+    def update_needle():
+        gauge.set_angle(gauge.angle + 1)
+        root.after(10, update_needle)
 
-    update_gauge()
-    print("a")
+    update_needle()
 
     root.mainloop()
