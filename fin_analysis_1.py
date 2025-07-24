e39da1b570469cc7a9b00f2b790d7d72ab31d0c7
Jeff Cherry <jeff_wired@yahoo.com>
2025-07-24 09:29:17 -0700

    New Versions

--- fin_analysis_1.py
+++ fin_analysis_1.py
@@ -2,18 +2,20 @@ import tkinter as tk
 from tkinter import filedialog
 import csv
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 # Suppress root window
 root = tk.Tk()
 root.withdraw()
 
-def find_data(biller, start_date, end_date, max):
+def find_data(search, start_date, end_date, max):
     number_withdrawals = 0
     total_amount = 0
     lines = []
     for i in range(num_rows):
         # print(data_list[i])
-        if biller in (data_list[i]['Description']):
+        if search in (data_list[i]['Description']):
             if number_withdrawals < max:
                 target_date = datetime.strptime((data_list[i]['Date']), "%m/%d/%y")
                 if start_date <= target_date <= end_date:
@@ -56,43 +58,36 @@ file = open("accout.txt", "w")
 num_rows = len(data_list)
 print(f"Number of rows: {num_rows}")
 
+today = datetime.now()
+start_date = today - relativedelta(years=1)
+# start_date = datetime.strptime("07/31/24", "%m/%d/%y")
+end_date = today
+# new_date = start_date - relativedelta(years=1)
+print(start_date,end_date,today)
 
-start_date = datetime.strptime("01/01/24", "%m/%d/%y")
-end_date = datetime.strptime("06/30/25", "%m/%d/%y")
 cc_total = 0
-ed_total = 0
-gr_total = 0
-sm_total = 0
-
+cc_average = 0
 
 with open("input.csv", newline="") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        biller = row['Biller']
+        search = row['Search']
+        name = row['Name']
         file.write("\n")
-        file.write(biller + "\n")
-        withdrawals, total, lines = find_data(biller, start_date, end_date, int(row['MAX']))
+        file.write(name + "\n")
+        withdrawals, total, lines = find_data(search, start_date, end_date, int(row['MAX']))
         for line in lines:
             file.write(line + "\n")
-        file.write(f"Biller:{biller}, #:{withdrawals}, Total:{total}" + "\n")
+        file.write(f"{name}, #:{withdrawals}, Total:{int(total)},  Average:{int(total/withdrawals)}" + "\n")
         if row['TYPE'] == "CC":
             cc_total += total
-        if row['TYPE'] == "ED":
-            ed_total += total
-        if row['TYPE'] == "GR":
-            gr_total += total
-        if row['TYPE'] == "SM":
-            sm_total += total
+            cc_average += total/withdrawals
 
 file.write("\n")
 s = start_date.strftime("%m/%d/%Y")
 e = end_date.strftime("%m/%d/%Y")
 file.write(f"Start Date:{s}, End Date:{e}" + "\n\n")
 file.write(f"Total Credit Cards = {int(cc_total)}" + "\n")
+file.write(f"Total Credit Cards Average= {int(cc_average)}" + "\n")
 file.write(f"Total Credit Cards Monthly = {int(cc_total/12)}" + "\n\n")
-file.write(f"Total Edison = {int(ed_total)}" + "\n")
-file.write(f"Total Edison Monthly = {int(ed_total/12)}" + "\n\n")
-file.write(f"Total Golden Rain = {int(gr_total)}" + "\n")
-file.write(f"Total Golden Rain Monthly = {int(gr_total/12)}" + "\n\n")
-file.write(f"Total State Of Michigan = {int(sm_total)}" + "\n")
-file.write(f"Total State Of Michigan Monthly = {int(sm_total/12)}" + "\n\n")
+
